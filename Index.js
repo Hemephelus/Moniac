3105819720e09e0f0df376debe72cd52f0502174
Hemephelus <nwachukwuujubuonu@gmail.com>
2023-12-24 07:24:09 +0100

    setup new zooming and panning

--- Index.js
+++ Index.js
@@ -1,34 +1,34 @@
-  // Economics
-  let TAX_RATE = 0.5;
-  let TRANSACTION_BALANCE = 2000;
-  let GOVERNMENT_EXPENDITURE_RATE = 1.0;
-  let AVERAGE_PROPENSITY_TO_CONSUME = 0.5;
-  let INVESTMENT_RATE = 1.0;
-  let AVERAGE_PROPENSITY_IMPORT = 0.0;
-  //   let AUTONOMOUS_GOVERNMENT_EXPENDITURE = 0;
-  //   let GDP_Y = TRANSACTION_BALANCE;
-  //   let AUTONOMOUS_EXPORT = 0;
-  //   let GOVERNMENT_SURPLUS = 0;
-  //   let IDLE_BALANCE = 0;
-  //   let IMPORT_AMOUNT = 0;
-  //   let EXPORT_AMOUNT = 0;
-  // Economics
+//   // Economics
+//   let TAX_RATE = 0.5;
+//   let TRANSACTION_BALANCE = 2000;
+//   let GOVERNMENT_EXPENDITURE_RATE = 1.0;
+//   let AVERAGE_PROPENSITY_TO_CONSUME = 0.5;
+//   let INVESTMENT_RATE = 1.0;
+//   let AVERAGE_PROPENSITY_IMPORT = 0.0;
+//   //   let AUTONOMOUS_GOVERNMENT_EXPENDITURE = 0;
+//   //   let GDP_Y = TRANSACTION_BALANCE;
+//   //   let AUTONOMOUS_EXPORT = 0;
+//   //   let GOVERNMENT_SURPLUS = 0;
+//   //   let IDLE_BALANCE = 0;
+//   //   let IMPORT_AMOUNT = 0;
+//   //   let EXPORT_AMOUNT = 0;
+//   // Economics
 
-  let debug = false;
-  // A path object (series of connected points)
-  let paths = [];
-  // Two vehicles
-  let vehicles = [];
-  let boxes = [];
-  let mouseLocation;
-  let offset = 30;
+//   let debug = false;
+//   // A path object (series of connected points)
+//   let paths = [];
+//   // Two vehicles
+//   let vehicles = [];
+//   let boxes = [];
+//   let mouseLocation;
+//   let offset = 30;
 
-  let endAnchors = [];
-  let k = 0
-  let canvas
+//   let endAnchors = [];
+//   let k = 0
+//   let canvas
 
 
-  let mouseStates = {
-    zoom_pan_tool: false,
-    selection_tool: true,
-  }
+//   let mouseStates = {
+//     zoom_pan_tool: false,
+//     selection_tool: true,
+//   }
